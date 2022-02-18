53170fea3dfbb83b7e2f1e485276f34b0f6ec1e5
IT21278280 <99583379+IT21278280@users.noreply.github.com>
2022-02-18 21:44:30 +0000

    What did you change

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,20 @@
 #include <stdio.h>
 
 int main() {
+
+  int m1,m2,sum=0;
+  float avg;
+  
+  printf("Enter subject 1 mark:");
+  scanf("%d",&m1);
+
+  printf("Enter subject 2 mark:");
+  scanf("%d",&m2);
+
+  sum = m1 + m2;
+  avg = (float)sum/2 ;
+
+  printf("Average = %.2f",avg);
   
   return 0;
 }
--- Tute04.c
+++ Tute04.c
@@ -6,6 +6,10 @@ Do not change the code given in the main() function when you are implementing yo
 
 #include <stdio.h>
 
+int minimum(int num1, int num2);
+int maximum(int num1, int num2);
+int multiply(int num1, int num2);
+
 int main() {
    int no1, no2;
    printf("Enter a value for no 1 : ");
@@ -16,4 +20,18 @@ int main() {
    printf("%d ", maximum(no1, no2));
    printf("%d ", multiply(no1, no2));
    return 0;
+}
+
+int minimum(int num1, int num2)
+{
+  return (num1 > num2 ) ? num2 : num1;
+}
+int maximum(int num1, int num2)
+{
+  return (num1 > num2 ) ? num1 : num2;
+}
+
+int multiply(int num1, int num2)
+{
+  return num1*num2;
 }
